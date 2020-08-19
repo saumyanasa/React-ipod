e750fe376fb7357353a061eb37df4d371e94ac45
Saumya Nasa <51260022+saumyanasa@users.noreply.github.com>
2020-08-19 22:46:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Body from './Body';
+
+class App extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <Body />
+      </div>
+    )
+    
+  }
+
+}
+
+export default App;
---
+++ Body.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Wheel from './Wheel';
+
+class Body extends React.Component{
+    constructor(){
+      super();
+    }
+  
+    render(){
+      return(
+        <div>
+          <Wheel />
+        </div>
+      )
+      
+    }
+  
+  }
+  
+  export default Body;
+  
